05f025bc6216c132b6817de22d345337a8378b97
Daniel <danielcarrion3000@gmail.com>
2026-03-19 23:45:57 -0500

    Clase usuario

--- gestiondetareas.py
+++ gestiondetareas.py
@@ -17,3 +17,22 @@ class Tarea:
         self.titulo =nuevo_titulo
         self.descripcion = nueva_descripcion
         self.fecha_vencimiento = nueva_fecha
+
+#clase usuario
+class Usuario:
+    def __init__(self, nombre_usuario, contrasena):
+        self.nombre_usuario = nombre_usuario
+        self.contrasena = contrasena
+        self.tareas = []
+
+    def agregar_tarea(self, tarea):
+        self.tareas.append(tarea)
+
+    def eliminar_tarea(self, titulo_tarea):
+        self.tareas = [tarea for tarea in self.tareas if tarea.titulo !=titulo_tarea] 
+    #recorre toda la lista de tareas hasta llegar a la tarea que tenga el titulo igual al ingresado
+
+
+    def obtener_tareas(self):
+        return self.tareas
+    
